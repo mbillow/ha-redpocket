a1211d9e81292e6e132a4f24f1de19d6be2af73d
Marc Billow <mbillow@me.com>
2021-05-21 15:07:40 -0500

    Logging and drastically reduce API calls

--- custom_components/ha-redpocket/sensor.py
+++ custom_components/ha-redpocket/sensor.py
@@ -1,10 +1,8 @@
 """Sensor platform for Mint Mobile."""
 import datetime
 import logging
-import numbers
-import uuid
 from datetime import timedelta
-from typing import Optional
+from typing import List
 
 from homeassistant.const import (
     CONF_PASSWORD,
@@ -13,12 +11,15 @@ from homeassistant.const import (
     STATE_UNKNOWN,
 )
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 from redpocket import RedPocket, RedPocketAuthError, RedPocketException
 from redpocket.api import RedPocketLine, RedPocketLineDetails
 
-from .const import DEFAULT_NAME, DEFAULT_SCAN_INTERVAL, DOMAIN, ICON, SENSOR
+from .const import DEFAULT_NAME, DEFAULT_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,38 +36,80 @@ async def async_setup_entry(hass, entry, async_add_entities):
     password = config.get(CONF_PASSWORD)
     rp = await hass.async_add_executor_job(RedPocket, username, password)
 
-    lines = await hass.async_add_executor_job(rp.get_lines)
+    lines: List[RedPocketLine] = await hass.async_add_executor_job(rp.get_lines)
+    _LOGGER.info("Found %d associated lines with RedPocket account.", len(lines))
+
     for line in lines:
-        sensors.append(RedPocketVoiceBalanceSensor(hass, line))
-        sensors.append(RedPocketMessagingBalanceSensor(hass, line))
-        sensors.append(RedPocketDataBalanceSensor(hass, line))
-        sensors.append(RedPocketRemainingDaysSensor(hass, line))
-        sensors.append(RedPocketRemainingMonthsSensor(hass, line))
+
+        async def async_update_data() -> RedPocketLineDetails:
+            """Fetch data from API endpoint.
+
+            This is the place to pre-process the data to lookup tables
+            so entities can quickly look up their data.
+            """
+            _LOGGER.debug(
+                "Refreshing line details for line: %s (%s)",
+                line.number,
+                line.account_id,
+            )
+            try:
+                # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+                # handled by the data update coordinator.
+                return await hass.async_add_executor_job(line.get_details)
+            except RedPocketAuthError as err:
+                _LOGGER.error("Unable to update line data, invalid credentials!")
+                raise UpdateFailed from err
+            except RedPocketException as err:
+                _LOGGER.exception("Unable to update line data, unknown error!")
+                raise UpdateFailed from err
+
+        coordinator = DataUpdateCoordinator(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name=f"redpocket line {line.number} sensor",
+            update_method=async_update_data,
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(minutes=DEFAULT_SCAN_INTERVAL),
+        )
+
+        _LOGGER.info(
+            "Configuring sensors for RedPocket Line: %d (%s)",
+            line.number,
+            line.account_id,
+        )
+        _LOGGER.debug(line)
+        sensors.append(RedPocketVoiceBalanceSensor(hass, line, coordinator))
+        sensors.append(RedPocketMessagingBalanceSensor(hass, line, coordinator))
+        sensors.append(RedPocketDataBalanceSensor(hass, line, coordinator))
+        sensors.append(RedPocketRemainingDaysSensor(hass, line, coordinator))
+        sensors.append(RedPocketRemainingMonthsSensor(hass, line, coordinator))
 
     async_add_entities(sensors, True)
 
 
-class RedPocketBaseSensor(Entity):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+class RedPocketBaseSensor(CoordinatorEntity):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
+        super().__init__(coordinator)
 
         # Home Assistant Constants
         self.hass = hass
-        self._scan_interval = timedelta(minutes=DEFAULT_SCAN_INTERVAL)
-        _LOGGER.debug("Config scan interval: %s", self._scan_interval)
 
         # Sensor Defaults
         self._id_suffix = "unknown"
+        self._detail_key = ""
         self._name = "Unknown RedPocket Sensor"
         self._icon = "mdi:border-none-variant"
         self._unit_of_measurement = None
         self._state = STATE_UNKNOWN
-        self._last_updated = self._current_time()
 
         # Red Pocket Line Information
         self._line: RedPocketLine = line
-
-        # Schedule Update
-        self.async_update = Throttle(self._scan_interval)(self.async_update)
 
     @property
     def unique_id(self):
@@ -93,151 +136,110 @@ class RedPocketBaseSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        if self._state == -1:
+        if (
+            not self.coordinator.data
+            or not self._detail_key
+            or getattr(self.coordinator.data, self._detail_key) == -1
+        ):
             return STATE_UNAVAILABLE
-        return self._state
-
-    @property
-    def device_state_attributes(self):
-        """Return device specific state attributes."""
-        return {"last_updated": self._last_updated}
+        return getattr(self.coordinator.data, self._detail_key)
 
     @staticmethod
     def _current_time() -> str:
         """Return current timestamp"""
         return datetime.datetime.now().strftime("%b-%d-%Y %I:%M %p")
 
-    async def _async_get_line_details(self) -> Optional[RedPocketLineDetails]:
-        """
-        Fetch new state data for the sensor.
-        """
-
-        # Pull additional information from APIs
-        try:
-            details = await self.hass.async_add_executor_job(self._line.get_details)
-            self._last_updated = self._current_time()
-            return details
-        except RedPocketAuthError:
-            _LOGGER.error("Unable to update line data, invalid credentials!")
-        except RedPocketException:
-            _LOGGER.exception("Unable to update line data, unknown error!")
-
 
 class RedPocketDataBalanceSensor(RedPocketBaseSensor):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
 
         # Initialize Defaults
-        super().__init__(hass, line)
+        super().__init__(hass, line, coordinator)
 
         # Sensor Overrides
         self._id_suffix = "data_balance"
         self._name = f"{line.number} High-Speed Data Balance"
         self._icon = "mdi:signal-4g"
         self._unit_of_measurement = "MB"
-
-    async def async_update(self):
-        """
-        Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-
-        # Pull additional information from APIs
-        update = await self._async_get_line_details()
-        if isinstance(update, RedPocketLineDetails):
-            self._state = update.data_balance
+        self._detail_key = "data_balance"
 
 
 class RedPocketVoiceBalanceSensor(RedPocketBaseSensor):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
 
         # Initialize Defaults
-        super().__init__(hass, line)
+        super().__init__(hass, line, coordinator)
 
         # Sensor Overrides
         self._id_suffix = "voice_balance"
         self._name = f"{line.number} Voice Balance"
         self._icon = "mdi:account-voice"
         self._unit_of_measurement = "Minutes"
-
-    async def async_update(self):
-        """
-        Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-
-        # Pull additional information from APIs
-        update = await self._async_get_line_details()
-        if isinstance(update, RedPocketLineDetails):
-            self._state = update.voice_balance
+        self._detail_key = "voice_balance"
 
 
 class RedPocketMessagingBalanceSensor(RedPocketBaseSensor):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
 
         # Initialize Defaults
-        super().__init__(hass, line)
+        super().__init__(hass, line, coordinator)
 
         # Sensor Overrides
         self._id_suffix = "messaging_balance"
         self._name = f"{line.number} Messaging Balance"
         self._icon = "mdi:message-processing"
         self._unit_of_measurement = "Messages"
-
-    async def async_update(self):
-        """
-        Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-
-        # Pull additional information from APIs
-        update = await self._async_get_line_details()
-        if isinstance(update, RedPocketLineDetails):
-            self._state = update.messaging_balance
+        self._detail_key = "messaging_balance"
 
 
 class RedPocketRemainingDaysSensor(RedPocketBaseSensor):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
 
         # Initialize Defaults
-        super().__init__(hass, line)
+        super().__init__(hass, line, coordinator)
 
         # Sensor Overrides
         self._id_suffix = "remaining_days"
         self._name = f"{line.number} Days Remaining In Cycle"
         self._icon = "mdi:timer-sand"
         self._unit_of_measurement = "Days"
-
-    async def async_update(self):
-        """
-        Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-
-        # Pull additional information from APIs
-        update = await self._async_get_line_details()
-        if isinstance(update, RedPocketLineDetails):
-            self._state = update.remaining_days_in_cycle
+        self._detail_key = "remaining_days_in_cycle"
 
 
 class RedPocketRemainingMonthsSensor(RedPocketBaseSensor):
-    def __init__(self, hass: HomeAssistant, line: RedPocketLine):
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        line: RedPocketLine,
+        coordinator: DataUpdateCoordinator,
+    ):
 
         # Initialize Defaults
-        super().__init__(hass, line)
+        super().__init__(hass, line, coordinator)
 
         # Sensor Overrides
         self._id_suffix = "remaining_months"
         self._name = f"{line.number} Months Purchased Left"
         self._icon = "mdi:calendar-month"
         self._unit_of_measurement = "Months"
-
-    async def async_update(self):
-        """
-        Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-
-        # Pull additional information from APIs
-        update = await self._async_get_line_details()
-        if isinstance(update, RedPocketLineDetails):
-            self._state = update.remaining_months_purchased
+        self._detail_key = "remaining_months_purchased"
